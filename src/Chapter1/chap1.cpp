e92ea949d28f2142fd8e155882fdf2d2ab6b7cff
YX.E.Z <zhouyixuan29@gmail.com>
2021-06-18 11:12:37 +0800

    update with linear calibrate

--- src/Chapter1/chap1.cpp
+++ src/Chapter1/chap1.cpp
@@ -1,9 +1,23 @@
 #include "chap1.h"
-#include <Eigen/Core>
 
-void linear_calibrate() {
-    Eigen::Vector3d test_vector{1, 2, 3};
-    std::cout << "check I'm in chap1" << test_vector << std::endl;
+CameraParameters linear_calibrate(const Eigen::Matrix2Xd &pixel_coordinates, const Eigen::Matrix3Xd &real_coordinates) {
+    int min_pairs = (int)std::min(pixel_coordinates.cols(), real_coordinates.cols());
+    assert(min_pairs > 6);
+
+    /*Estimation Project Matrix*/
+    Eigen::MatrixXd P;
+    P.resize(2*min_pairs,2*min_pairs);
+    for(int i=0;i<min_pairs;++i){
+        P.row(i+0)<<real_coordinates.col(i),1,0,0,0,0,-pixel_coordinates.col(i)[0]*real_coordinates.col(i),-pixel_coordinates.col(i)[0];
+        P.row(i+1)<<0,0,0,0,real_coordinates.col(i),1,-pixel_coordinates.col(i)[1]*real_coordinates.col(i),-pixel_coordinates.col(i)[1];
+    }
+    Eigen::VectorXd b;
+    b.resize(2*min_pairs,1);
+    b.setConstant(0);
+    Eigen::MatrixXd m{P.jacobiSvd(Eigen::ComputeThinU|Eigen::ComputeThinV).solve(b)};
+    m.conservativeResize(3,4);
+
+
 }
 
 void non_linear_calibrate() {
--- src/Chapter1/chap1.h
+++ src/Chapter1/chap1.h
@@ -1,16 +1,44 @@
 /**
- *
+ * Chapter 1 contains two applications: linear calibrate and non-linear calibrate
  */
 #ifndef CV_MP_CHAP1_H
 #define CV_MP_CHAP1_H
 
 #include <iostream>
+#include <Eigen/Dense>
+
+struct CameraParameters {
+public:
+    Eigen::Matrix3d K;
+    Eigen::Matrix3d R;
+    Eigen::Vector3d t;
+
+    CameraParameters()
+            : K(Eigen::Matrix3d::Zero()),
+              R(Eigen::Matrix3d::Identity()),
+              t(Eigen::Vector3d::Zero()) {
+    };
+    Eigen::Matrix<double,3,4> getProjectMatrix(){
+        Eigen::Matrix<double,3,4> extrinsic_temp_matrix;
+        extrinsic_temp_matrix<<R,t;
+        return Eigen::Matrix<double,3,4>{K*extrinsic_temp_matrix};
+    }
+    Eigen::Matrix4d getExtrinsicMatrix(){
+        Eigen::Matrix4d results;
+        results<<R,t,0,0,0,1;
+        return results;
+    }
+};
 
 /**
- *
+ * @brief estimation of camera intrinsic and extrinsic parameters with linear method,
+ *        at least need 6 non-coplane correspondences
+ * @source Section 1.3.1
+ * @param pixel_coordinates: 2 X n pixel coordinates, n must bigger than 6
+ * @param real_coordinates: 3 X n real coordinates. Each column is a point [X,Y,Z];
  */
-void linear_calibrate();
+CameraParameters linear_calibrate(const Eigen::Matrix2Xd &pixel_coordinates, const Eigen::Matrix3Xd &real_coordinates);
 
-void non_linear_calibrate();
+CameraParameters non_linear_calibrate(const Eigen::Matrix2Xd &pixel_coordinates, const Eigen::Matrix3Xd &real_coordinates);
 
 #endif //CV_MP_CHAP1_H
